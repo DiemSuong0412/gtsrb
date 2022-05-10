770a33ad1c23764d3d547eb83027052f2bf70ab5
DiemSuong0412 <19522145@gm.uit.edu.vn>
2022-05-10 22:24:53 +0700

    recognition

---
+++ gui.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+
+model = load_model('cnn_clf.h5')
+
+# Các nhãn của biển báo
+classNames = {0: 'Speed limit (20km/h)',
+              1: 'Speed limit (30km/h)',
+              2: 'Speed limit (50km/h)',
+              3: 'Speed limit (60km/h)',
+              4: 'Speed limit (70km/h)',
+              5: 'Speed limit (80km/h)',
+              6: 'End of speed limit (80km/h)',
+              7: 'Speed limit (100km/h)',
+              8: 'Speed limit (120km/h)',
+              9: 'No passing',
+              10: 'No passing for vehicles over 3.5 metric tons',
+              11: 'Right-of-way at the next intersection',
+              12: 'Priority road',
+              13: 'Yield',
+              14: 'Stop',
+              15: 'No vehicles',
+              16: 'Vehicles over 3.5 metric tons prohibited',
+              17: 'No entry',
+              18: 'General caution',
+              19: 'Dangerous curve to the left',
+              20: 'Dangerous curve to the right',
+              21: 'Double curve',
+              22: 'Bumpy road',
+              23: 'Slippery road',
+              24: 'Road narrows on the right',
+              25: 'Road work',
+              26: 'Traffic signals',
+              27: 'Pedestrians',
+              28: 'Children crossing',
+              29: 'Bicycles crossing',
+              30: 'Beware of ice/snow',
+              31: 'Wild animals crossing',
+              32: 'End of all speed and passing limits',
+              33: 'Turn right ahead',
+              34: 'Turn left ahead',
+              35: 'Ahead only',
+              36: 'Go straight or right',
+              37: 'Go straight or left',
+              38: 'Keep right',
+              39: 'Keep left',
+              40: 'Roundabout mandatory',
+              41: 'End of no passing',
+              42: 'End of no passing by vehicles over 3.5 metric tons'}
+
+# Tạo giao diện
+top = tk.Tk()
+top.geometry('800x600')
+top.title('Hệ thống nhận dạng biển báo giao thông')
+top.configure(background='#CDCDCD')
+label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
+sign_image = Label(top)
+
+
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((32, 32))
+    image = np.expand_dims(image, axis=0)
+    image = np.array(image)
+    print("Chiều của ảnh: ", image.shape)
+    Y_pred = model.predict([image])[0]
+    print("Kết quả dự đoán \n", Y_pred)
+    index = np.argmax(Y_pred)
+    sign = classNames[index]
+    print(sign)
+    label.configure(foreground='#011638', text=sign)
+
+
+def show_classify_button(file_path):
+    classify_b = Button(top, text='Nhận diện', command=lambda: classify(file_path), padx=10, pady=5)
+    classify_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+    classify_b.place(relx=0.79, rely=0.46)
+
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width() / 2.25), (top.winfo_height() / 2.25)))
+        im = ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image = im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+
+upload = Button(top, text="Tải ảnh lên", command=upload_image, padx=10, pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+upload.pack(side=BOTTOM, pady=50)
+sign_image.pack(side=BOTTOM, expand=True)
+label.pack(side=BOTTOM, expand=True)
+heading = Label(top, text="Nhận dạng biển báo giao thông", pady=20, font=('arial', 20, 'bold'))
+heading.configure(background='#CDCDCD', foreground='#364156')
+heading.pack()
+top.mainloop()
